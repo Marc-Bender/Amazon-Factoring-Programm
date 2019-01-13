989b6c09041121564ec44c817ceae271cda2e471
Marc Bender <marc.andre.bender@hotmail.com>
2019-01-13 02:06:48 +0100

    auch dateien mit leerzeichen werden jetzt verarbeitet es sind erfolge mit rechnungs und amazon ordernummer vorzuweisen
    TODO: In Textdatei rausschreiben statt in msgbox anzeigen

--- WpfApp1/MainWindow.xaml.vb
+++ WpfApp1/MainWindow.xaml.vb
@@ -13,8 +13,46 @@ Class MainWindow
         chooseFilesDialog.Title = "Datei(-en) wählen"
         chooseFilesDialog.Multiselect = True
         chooseFilesDialog.ShowDialog()
-        For i = 0 To chooseFilesDialog.FileNames.Length - 1
-            Process.Start("pdftotext", chooseFilesDialog.FileNames(i))
-        Next
+        Dim numOfFilesSelected As Integer = chooseFilesDialog.FileNames.Length
+        If numOfFilesSelected > 0 Then
+            statusMessages.AppendText(Chr(13) + "Ausgewählt " + numOfFilesSelected.ToString + " Dateien")
+            For i = 0 To numOfFilesSelected - 1
+                Dim oldFilename = chooseFilesDialog.FileNames(i)
+                Dim newFilename = oldFilename.Replace(" ", "")
+                Rename(oldFilename, newFilename)
+                Threading.Thread.Sleep(100)
+                Dim pdftotextProcess = Process.Start("pdftotext", newFilename)
+                pdftotextProcess.WaitForExit()
+                Dim textFilename = newFilename.Replace(".pdf", ".txt")
+                Dim myfile = File.OpenRead(textFilename)
+                Dim invoicenumber As String
+                Dim amazonordernumber As String
+                Dim myline As String
+                For j = 0 To myfile.Length - 1
+                    Dim mybyte = Chr(myfile.ReadByte).ToString
+                    If Not (mybyte.Equals(vbCrLf)) AndAlso Not (mybyte.Equals(vbCr)) AndAlso Not (mybyte.Equals(vbLf)) Then
+                        myline += mybyte
+                    Else
+                        If myline.Contains("Rechnungsnr. ") Then
+                            invoicenumber = myline.Remove(0, "Rechnungsnr. ".Length)
+                            Dim sampleinvoicenumber = "184183"
+                            invoicenumber = invoicenumber.Remove(sampleinvoicenumber.Length, invoicenumber.Length - sampleinvoicenumber.Length)
+                        End If
+                        If myline.Contains("Amazon Order ") Then
+                            amazonordernumber = myline.Remove(0, "Amazon Order ".Length)
+                            Dim sampleamazonordernumber = "302-6384101-1861149"
+                            amazonordernumber = amazonordernumber.Remove(sampleamazonordernumber.Length, amazonordernumber.Length - sampleamazonordernumber.Length)
+                        End If
+                        myline = ""
+                        End If
+
+                Next
+                MsgBox("rechnungsnummer " + invoicenumber.ToString)
+                MsgBox("ordernummer " + amazonordernumber.ToString)
+                Rename(newFilename, oldFilename)
+                myfile.Close()
+                My.Computer.FileSystem.DeleteFile(textFilename)
+            Next
+        End If
     End Sub
 End Class
